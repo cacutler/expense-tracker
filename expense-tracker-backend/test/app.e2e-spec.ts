9b3a6a981275967a0f24816f6e22b771ac7f3322
cacutler <116305038+cacutler@users.noreply.github.com>
2026-01-21 08:50:50 -0700

    Updated code styling

--- expense-tracker-backend/test/app.e2e-spec.ts
+++ expense-tracker-backend/test/app.e2e-spec.ts
@@ -6,9 +6,7 @@ import { AppModule } from './../src/app.module';
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
   beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+    const moduleFixture: TestingModule = await Test.createTestingModule({imports: [AppModule]}).compile();
     app = moduleFixture.createNestApplication();
     await app.init();
   });
